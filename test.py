18c3a1b5d221da1f25cfabc5810f847d1a0f6a2c
Alex-Kout <62557059+Alex-Kout@users.noreply.github.com>
2023-02-20 18:53:16 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,14 +1,17 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-from pandas import ExcelWriter
-from pandas import ExcelFile
+
 
 st.header("""
 test
 """)
 
-path = 'https://raw.githubusercontent.com/Alex-Kout/Book_market_analysis/main/publisher.xlsx'
+path = r'https://raw.githubusercontent.com/Alex-Kout/Book_market_analysis/main/publisher.xlsx'
+
+# Function to get the unique values to filter   
+
+###-----------------SIdeBar--------------------------------
 
 
 data = pd.read_excel(path)
